7b2b5ceb9b24c47b868984f8a0685b8fc5fe1df2
Michael55555 <michaelpeters5647@gmail.com>
2019-06-24 22:10:29 +0200

    Add help command

--- src/index.js
+++ src/index.js
@@ -9,4 +9,13 @@ if (!token) {
 
 client.on("voiceStateUpdate", require("./VoiceUpdate"));
 
+client.on('message', async (msg) => {
+  if (msg.mentions.members.first().id === client.user.id) {
+    const embed = new RichEmbed()
+      .setTitle('Help')
+      .addField('^@Viav', 'Sends I this message')
+    const dm = await msg.author.createDM()
+    dm.send({ embed })
+  }
+})
 client.login(token);
